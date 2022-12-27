b1f24e01ebdaa803ae8e38583cbeb7b66ff9dcf8
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-27 14:10:52 +0700

    SiGG-FinancialTech-Griffin-GM.ClientAPI-Quater-CounterPartyEndpoint

---
+++ CounterPartyEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using GM.ClientAPI.Endpoint.CounterParty;
+
+namespace GM.ClientAPI.Endpoint
+{
+    public class CounterPartyEndpoint
+    {
+        public CounterPartyAPI CounterParty { get; }
+
+        public CounterPartyEndpoint(IHttpClientFactory httpClientFactory)
+        {
+            var client = httpClientFactory.CreateClient("CounterPartyEndpoint");
+            CounterParty = new CounterPartyAPI(client);
+        }
+    }
+}
